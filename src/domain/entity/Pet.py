c9873049672980fb69aba7e5edb3d6c9616454e1
Miguel de  Oliveira <migvanderlei@gmail.com>
2022-08-18 01:36:49 -0400

    first commit

---
+++ src/domain/entity/Pet.py
@@ -0,0 +1,13 @@
+class Pet:
+    def __init__(self,
+        id_=None, name=None, sex=None, species = None,
+        weight=None, date_of_birth = None, blood_type = None, owner = None):
+
+        self.id = id
+        self.name = name
+        self.sex = sex
+        self.species = species
+        self.weight = weight
+        self.date_of_birth = date_of_birth
+        self.blood_type = blood_type
+        self.owner = owner
---
+++ src/infra/client/MongoDbClient.py
@@ -0,0 +1,16 @@
+import os
+from pymongo import MongoClient
+
+MONGODB_CONNECTION_STRING = os.environ.get('MONGODB_CONNECTION_STRING')
+MONGODB_USER = os.environ.get('MONGODB_USER')
+MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
+
+class MongoDbClient:
+
+    def __init__(self):
+        # Provide the mongodb atlas url to connect python to mongodb using pymongo
+        self.connection_string = MONGODB_CONNECTION_STRING.format(MONGODB_USER, MONGODB_PASSWORD)
+
+    def get_database(self, database_name='hemopet'):
+        client = MongoClient(self.connection_string)
+        return client[database_name]
